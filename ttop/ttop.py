c4df4be039bc259b5924bbf111538a4a5f070815
ton <tonton1517@gmail.com>
2013-09-30 15:43:01 +0900

    implements view mode options. closed #1

--- ttop/ttop.py
+++ ttop/ttop.py
@@ -6,7 +6,7 @@ ttop is CUI graphical system monitor.
 this tools is designed for use with tmux(or screen).
 
 Usage:
-  ttop [--no-color] [--interval <s>]
+  ttop [--no-color] [--interval <s>] [normal | minimal | stack] [horizontal | vertical]
   ttop -h | --help
   ttop -v | --version
 
@@ -40,6 +40,15 @@ EXIT_KEYS = (ord("q"), ord("Q"), 27) # 27:ESC
 # Functions
 #=======================================
 
+def init_arguments(arg):
+    # default view is "normal".
+    if not arg["normal"] and not arg["minimal"] and not arg["stack"]:
+        arg["normal"] = True
+
+    # default style is "horizontal".
+    if not arg["horizontal"] and not arg["vertical"]:
+        arg["horizontal"] = True
+
 def init_curses():
     """must be called in hook_curses function."""
 
@@ -49,6 +58,31 @@ def init_curses():
     ## hide cursor
     curses.curs_set(0)
 
+def select_color_theme(arguments):
+    color_table = ColorTable()
+    return MonoColorTheme(color_table) if arguments["--no-color"] else DefaultColorTheme(color_table)
+
+def select_layout_class(arguments):
+    layout_class = None
+
+    if arguments["normal"] and arguments["horizontal"]:
+        layout_class = HorizontalDefaultLayout
+    elif arguments["minimal"] and arguments["horizontal"]:
+        layout_class = HorizontalMinimalLayout
+    elif arguments["stack"] and arguments["horizontal"]:
+        pass
+    elif arguments["normal"] and arguments["vertical"]:
+        pass
+    elif arguments["minimal"] and arguments["vertical"]:
+        pass
+    elif arguments["stack"] and arguments["vertical"]:
+        pass
+    else:
+        pass
+
+    return layout_class
+
+
 def start_process(scr, arguments):
     p = Process(target=update_handler, args=(scr, arguments))
     p.daemon = True
@@ -56,9 +90,15 @@ def start_process(scr, arguments):
 
 def update_handler(scr, arguments):
     ss = SystemStatus()
-    color_table = ColorTable()
-    theme = MonoColorTheme(color_table) if arguments["--no-color"] else DefaultColorTheme(color_table)
-    layout = HorizontalDefaultLayout(scr, theme, ss)
+    theme = select_color_theme(arguments)
+    layout_class = select_layout_class(arguments)
+
+    if layout_class is None:
+        scr.addstr("not yet implements.")
+        scr.refresh()
+        return
+
+    layout = layout_class(scr, theme, ss)
 
     interval = float(arguments["--interval"])
 
@@ -82,6 +122,8 @@ def hook_curses(scr, arguments):
 
 def main():
     arguments = docopt(__doc__, version=__version__)
+    init_arguments(arguments)
+
     curses.wrapper(hook_curses, arguments)
 
 if __name__ == "__main__":
