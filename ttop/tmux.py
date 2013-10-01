96a28884b0011b477d2ba63503b908fcddf1d963
ton <tonton1517@gmail.com>
2013-10-01 18:49:39 +0900

    tmux wrapper module.

---
+++ ttop/tmux.py
@@ -0,0 +1,41 @@
+import os
+import subprocess
+
+#=======================================
+# tmux 
+#=======================================
+
+def call(command):
+    """call tmux subcommand."""
+    subprocess.Popen("tmux " + command, shell=True, stdout=subprocess.PIPE)
+
+def in_tmux():
+    """if process is in tmux, return True."""
+    return bool(os.getenv("TMUX"))
+
+def swap_pane():
+    """swap current pane for previous pane."""
+    call("swap-pane -D")
+
+def move_last_pane():
+    """move focus from current pane to previous pane."""
+    call("last-pane")
+
+def resize_pane(width=None, height=None):
+    """resize pane"""
+    resize_option = ""
+
+    if width:  resize_option += " -x " + str(width+1)
+    if height: resize_option += " -y " + str(height+1)
+
+    call("resize-pane " + resize_option)
+
+def split_window(vertical=True, horizontal=False, command=None):
+    """new pane and exec command."""
+    option = "-v" if vertical else "-h"
+
+    if command:
+        option += " \"" + command + "\""
+
+    call("split-window " + option)
+
