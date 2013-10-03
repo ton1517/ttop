7a0df3a8832da555ac1ca6791270ef4e82c54663
ton <tonton1517@gmail.com>
2013-10-03 09:05:06 +0900

    version up to 0.6.2

--- ttop/__init__.py
+++ ttop/__init__.py
@@ -5,5 +5,5 @@ ttop is CUI graphical system monitr.
 __author__ = "ton1517"
 __email__ = "tonton1517@gmail.com"
 __homepage__ = "https://github.com/ton1517/ttop"
-__version__ = "0.6.1"
+__version__ = "0.6.2"
 __license__ = "MIT License"
