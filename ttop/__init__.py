6f309ad7587edf7784a36d7b6e6f7a9db56889d1
ton <tonton1517@gmail.com>
2015-06-24 00:58:10 +0900

    0.9.1

--- ttop/__init__.py
+++ ttop/__init__.py
@@ -5,5 +5,5 @@ ttop is CLI graphical system monitor.
 __author__ = "ton1517"
 __email__ = "tonton1517@gmail.com"
 __homepage__ = "https://github.com/ton1517/ttop"
-__version__ = "0.9.0"
+__version__ = "0.9.1"
 __license__ = "MIT License"
